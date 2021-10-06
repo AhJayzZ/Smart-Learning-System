fd5c88dd547387691fbe983f9201f085834eb9a9
AhJayzZ <antony95070@gmail.com>
2021-10-06 13:07:08 +0800

    add frame flip & now state & exit functions

--- project_codes/image_recognition/recognition_program.py
+++ project_codes/image_recognition/recognition_program.py
@@ -379,8 +379,8 @@ class RecognitionProgram:
 
         with mp_hands.Hands(
                 static_image_mode=False,
-                min_detection_confidence=0.7,
-                min_tracking_confidence=0.7,
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5,
                 max_num_hands=1)as hands:
 
             #self.cap = VideoSource(self._selected_camera)
--- project_codes/main_program.py
+++ project_codes/main_program.py
@@ -10,10 +10,6 @@ from GUI.languages import langauge_data
 import cv2
 import sys
 
-# PyQt signal object
-class Signal(QObject):
-    Signal = pyqtSignal()
-
 
 # 建立類別來繼承 Ui_SmartLearningSystemGUI 介面程式
 class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
@@ -28,8 +24,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.Recognition = RecognitionProgram()
         
         # Finish Flag setting(Avoid duplicated operation)
-        self.FinishFlag = Signal()
-        self.FinishFlag.Signal.connect(self.text_to_result_box)
+        self.FinishFlag = False
         
         # Camera setting
         camera_array = ['Camera 0(Webcam)', 'Camera 1(External Camera)']
@@ -53,6 +48,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         # Button trigger setting
         self.confirm_btn.clicked.connect(self.add_btn_click)
         self.clear_btn.clicked.connect(self.clear_btn_click)
+        self.exit_btn.clicked.connect(sys.exit)
         
         # Combobox trigger setting
         self.camera_selector.currentTextChanged.connect(self.camera_selector_changed)
@@ -69,22 +65,24 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.translated_box.clear()
         self.revise_textbox.clear()
 
-    # translate english text to translated language
+    # translate english text to translated language(too many request the translate function will disable)
     def translate(self):
         translator = Translator()
         text = self.result_box.toPlainText()
-        if len(text) >= 0 and type(text) != None :
+        try :
             result = translator.translate(text,dest=self.lang).text
             self.translated_box.setText(result)
+        except :
+            self.result_box.clear()
+            self.translated_box.clear()
 
     # change camera to the choosen one 
     def camera_selector_changed(self):
-        if self.camera_selector.currentIndex() == 0 :
-            self.Recognition._selected_camera = 0
+        index = self.camera_selector.currentIndex()
+        if index == 0 :
             self.Recognition.cap = cv2.VideoCapture(0)
-        elif self.camera_selector.currentIndex() == 1 :
-            self.Recognition._selected_camera = 1
-            self.Recognition.cap = cv2.VideoCapture(1)       
+        else :
+            self.Recognition.cap = cv2.VideoCapture(index,cv2.CAP_DSHOW)       
 
     # change to the translated langauge
     def languages_selector_changed(self):
@@ -92,17 +90,24 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.lang = self.languages_key_array[index]
         self.translate()
 
+    # flip the camera frame
+    def frame_flip(self):
+        print(self.frameDefault_btn.checked)
+
     # insert recognition text to result box
     def text_to_result_box(self):
         self.result_box.setText(self.Recognition.text)
 
     def refresh(self):
-        #print(self.Recognition.now_state)
         frame = self.Recognition.output_img
-        converted_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        if self.frameHorizontal_btn.isChecked() :
+            frame = cv2.flip(frame,1)
+        elif self.frameVertical_btn.isChecked() :
+            frame = cv2.flip(frame,0)
+        elif self.frameInverse_btn.isChecked() :
+            frame = cv2.flip(frame,-1)
 
-        # for webcam debug
-        # frame = cv2.flip(frame,1)
+        converted_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # PyQt image format
         height, width = converted_frame.shape[:2]
@@ -113,23 +118,33 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.camera_label.setPixmap(pyqt_img)
         self.camera_label.setScaledContents(True)
 
+        # Show now state
+        self.state_label.setText('現在狀態:' + str(self.Recognition.now_state))
+
         # Finish recognition and add text to result list
-        if self.Recognition.now_state == STATE.FinishRecognition:
-            self.FinishFlag.Signal.emit()
+        if self.Recognition.now_state == STATE.FinishRecognition :
+            if self.FinishFlag == False:
+                self.FinishFlag = True
+                print('Recognition text : ',self.Recognition.text)
+                self.result_box.setText(self.Recognition.text)
+        else :
+            self.FinishFlag = False
+        
 
         # Lightness warning 
         if self.Recognition.state_lightness == STATE_LIGHTNESS.TooBright:
             self.warning_label.setStyleSheet("color:red")
-            self.warning_label.setText('Warning:光線過亮')
+            self.warning_label.setText('光線狀態:光線過亮')
         elif self.Recognition.state_lightness == STATE_LIGHTNESS.TooDim:
             self.warning_label.setStyleSheet("color:red")
-            self.warning_label.setText('Warning:光線過暗') 
+            self.warning_label.setText('光線狀態:光線過暗') 
         else:
             self.warning_label.setStyleSheet("color:blue")
-            self.warning_label.setText('光線正常!')
+            self.warning_label.setText('光線狀態:正常!')
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     win = MainWindow()
     win.show()
     win.Recognition.run_program()
+    sys.exit(app.exec_())
