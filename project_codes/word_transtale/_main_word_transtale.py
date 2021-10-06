7ab8d4cdc3b47a4e3cc0f08d4f3d7d48f378455b
Winters369 <75155829+Winters369@users.noreply.github.com>
2021-10-06 18:04:56 +0800

    del: useless codes

--- project_codes/word_transtale/_main_word_transtale.py
+++
@@ -1,8 +0,0 @@
-from selector_TranslateOrWord import selector_TranslateOrWord
-
-def main(text):
-    try:
-        selector_TranslateOrWord(text)
-    except:
-        #print("something wrong with word_transtale")
-        pass
