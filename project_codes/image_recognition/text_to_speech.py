6e084a0fd886821afd06150a6e7b54d895f3a88b
AhJayzZ <antony95070@gmail.com>
2021-10-13 00:21:37 +0800

    add constrast & brightness adjust,add gtts functions

---
+++ project_codes/image_recognition/text_to_speech.py
@@ -0,0 +1,23 @@
+from gtts import gTTS
+from playsound import playsound
+import os
+
+
+def TextToSpeech(text):
+    """
+    input : some text or word
+    output : sound.mp3 of the text or word
+
+    convert text or word to mp3 file and save to current path
+    """
+    if len(text) > 0:
+        currentPath = os.path.dirname(__file__)
+        soundPath = os.path.join(currentPath,'sound.mp3')
+        tts = gTTS(str(text))
+        tts.save(soundPath)
+        playsound(soundPath)
+
+        # Avoid error(Permission denied)
+        os.remove(soundPath)
+    else :
+        print('gTTS empty word input')
