62093c06523d730b701bd726444911dee9ce071e
Winters369 <75155829+Winters369@users.noreply.github.com>
2021-10-20 14:42:54 +0800

    rename: crop->cropped

---
+++ project_codes/image_recognition/cropped_img_handler.py
@@ -0,0 +1,21 @@
+"""
+pocessing of the croped img, to be easier to recognition
+
+"""
+
+import cv2
+
+
+def cropped_img_handler(crop_img):
+    crop_img_new = cv2.cvtColor(crop_img, cv2.COLOR_RGB2GRAY)
+
+    crop_img_new.flags.writeable = True
+
+    crop_img_new = cv2.medianBlur(crop_img_new, 3)  # 除噪(降低邊緣雜訊)
+    crop_img_new = cv2.adaptiveThreshold(
+        crop_img_new, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)    # 自適應二值化
+    #crop_img_new = cv2.medianBlur(crop_img_new, 3)
+
+    crop_img_new.flags.writeable = False
+
+    return crop_img_new
--- project_codes/image_recognition/text_recognition.py
+++ project_codes/image_recognition/text_recognition.py
@@ -1,5 +1,5 @@
 import pytesseract
-from .crop_img_handler import crop_img_handler
+from .cropped_img_handler import cropped_img_handler
 from .text_correction import get_corrected_text
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -7,7 +7,7 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 def text_recognition(image_for_get_text):
     try:
-        image_for_get_text = crop_img_handler(image_for_get_text)
+        image_for_get_text = cropped_img_handler(image_for_get_text)
 
         text = pytesseract.image_to_string(image_for_get_text, lang='eng')
         text = get_corrected_text(text)
