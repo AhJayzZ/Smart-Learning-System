c189bc013495e3ab094fdbdc8f79124633e70f87
AhJayzZ <antony95070@gmail.com>
2021-10-29 18:49:55 +0800

    add translate history and open localDictionary function

--- project_codes/GUI/GUI_main.py
+++ project_codes/GUI/GUI_main.py
@@ -1,3 +1,4 @@
+from genericpath import isfile
 from os import path
 from re import S
 from PyQt5.QtWidgets import *
@@ -12,7 +13,7 @@ from word_transtale import selector_TranslateOrWord
 
 import numpy
 import cv2
-import sys
+import sys,os
 import pymysql
 import time
 
@@ -22,7 +23,6 @@ DB_USER = 'root'
 DB_PASSWORD = 'mitlab123456'
 DB_PORT = 3306
 DB_DATABASE = 'WordDB'
-
 
 class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
     """
@@ -36,11 +36,13 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.setWindowIcon(QIcon('./project_codes/GUI/images/GUI_icon.png'))
         self.sound_btn.setIcon(QIcon('./project_codes/GUI/images/sound_icon.png'))
         self.translate_btn.setIcon(QIcon('./project_codes/GUI/images/translate_icon.png'))
-        self.connectToDB()
+        self.connectDB_thread = connectDB_Thread()
+        self.connectDB_thread.start()
 
         # Recognition program
         self.contrast,self.brightness=1,0
         self.average_gray_value = 0
+        self.frameFlip = False
         self.Recognition = RecognitionProgram()
         self.frame_thread = frame_Thread(self.Recognition)
         self.frame_thread.frame_callback.connect(self.frame_refresh)
@@ -69,8 +71,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.lang = 'zh-tw'
         
         # Timer default setting
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.translate)
+        self.translateTimer = QTimer(self)
+        self.translateTimer.timeout.connect(self.translate)
         self.previousResult = ""
 
         # Result & translate box default setting
@@ -85,6 +87,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.sound_btn.clicked.connect(self.playSound)
         self.back_btn.clicked.connect(sys.exit) # Back to login page(unfinished)
 
+        # RadioButtin trigger default setting
+        self.frameDefault_btn.clicked.connect(self.frameMode_check)
+        self.frameHorizontal_btn.clicked.connect(self.frameMode_check)
+        self.frameVertical_btn.clicked.connect(self.frameMode_check)
+        self.frameInverse_btn.clicked.connect(self.frameMode_check)
+
         # Combobox trigger default setting
         self.camera_selector.currentTextChanged.connect(
             self.camera_selector_changed)
@@ -98,18 +106,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
             self.frame_contrast_brightness_check)
 
         # Menubar trigger default setting
-        self.settingAction = QAction(parent=self,text='設定')
-        self.settingAction.triggered.connect(self.settingAction_triggered)
-        self.guideAction = QAction(parent=self,text='使用手冊')
-        self.guideAction.triggered.connect(self.guideAction_triggered)
-        self.historyAction = QAction(parent=self,text='翻譯歷史')
-        self.historyAction.triggered.connect(self.historyAction_triggerred)
-        self.openWordFileAction = QAction(parent=self,text='打開單字本')
-        self.openWordFileAction.triggered.connect(self.openWordFileAction_triggered)
+        self.settingAction = QAction(parent=self,text='設定',triggered=self.settingAction_triggered)
+        self.guideAction = QAction(parent=self,text='使用手冊',triggered=self.guideAction_triggered)
+        self.openWordFileAction = QAction(parent=self,text='打開單字本',triggered=self.openWordFileAction_triggered)
         self.menubar.addAction(self.settingAction)
         self.menubar.addAction(self.guideAction)
-        self.menubar.addAction(self.historyAction)
         self.menubar.addAction(self.openWordFileAction)
+        
+        self.historyDict={}
+        self.historyAction = []
+        self.historyIndex = 0
+        self.historyMenu = QMenu(parent=self,title='翻譯紀錄')
+        self.menubar.addMenu(self.historyMenu)
+        
     
 # -----------------------------------------Window event--------------------------------------------
     def closeEvent(self,event):
@@ -155,10 +164,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
             else:                       # Word
                 data = self.result_box.toPlainText(), self.translation_output['defination']
                 
-            cursor = self.db.cursor()
+            cursor = self.connectDB_thread.db.cursor()
             mysql = "INSERT  INTO  SentenceTable (sentence,translation) VALUE ('%s','%s')" % data
             cursor.execute(mysql)
-            self.db.commit()
+            self.connectDB_thread.db.commit()
             print('Insert data to database success!')
         except:
             print('Insert data to database failed!')
@@ -168,7 +177,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         play .mp3 file of result_box text
         """
         self.input_text = self.result_box.toPlainText()
-        self.gTTS_thread = gTTS_Thread(self.input_text)
+        self.gTTS_thread = gTTS_Thread(self.input_text).start()
         self.gTTS_thread.start()
 
     def set_output_format(self):
@@ -177,6 +186,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         """
         self.sentenceOrWord = self.translation_thread.sentenceOrWord
         self.translation_output = self.translation_thread.translation_output
+
         if self.sentenceOrWord == 0: # Sentence
             self.translate_box.setText(self.translation_output)
         else :                       # Word
@@ -189,7 +199,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
                 try :
                     self.translate_box.setText('定義:\n' + self.translation_output['defination'])
                 except:
-                    self.translate_box.setText('Find Nothing,please try again!')   
+                    self.translate_box.setText('Find Nothing,please try again!') 
+        self.addTranslateHistory() 
                     
     def translate(self):
         """
@@ -206,14 +217,28 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
                 self.translation_thread = translation_Thread(self.input_text,self.lang)
                 self.translation_thread.translation_finished.connect(self.set_output_format)
                 self.translation_thread.start()
-                
+            
         self.previousResult = self.input_text
-    
+
+    def addTranslateHistory(self):
+        """
+        add the  action of translate history into menubar
+        """
+        print(self.historyIndex)
+        self.translateTimer.stop()
+        self.historyDict[self.input_text] = self.translate_box.toPlainText()
+        if self.historyIndex < 10:
+            self.historyAction.append(QAction(parent=self,text=self.input_text,triggered=self.historyAction_triggerred))
+            self.historyMenu.addAction(self.historyAction[self.historyIndex])
+        else:
+            self.historyAction[self.historyIndex % 10].setText(self.input_text)
+        self.historyIndex = self.historyIndex + 1
+        
     def translateTimeCount(self):
         """
         triggered counting for translation function
         """
-        self.timer.start(3000)
+        self.translateTimer.start(3000)
 
     def camera_selector_changed(self):
         """
@@ -236,16 +261,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.translate()
 
     # GUI camera frame check
-    def frame_check(self):
+    def frameMode_check(self):
         """
         change GUI frame  if frame flip or frame be lighten
         """
+        self.frameFlip = True
         if self.frameHorizontal_btn.isChecked():
-            self.frame_thread.frame = cv2.flip(self.frame_thread.frame, 1)
+            self.frameMode = 1
         elif self.frameVertical_btn.isChecked():
-            self.frame_thread.frame = cv2.flip(self.frame_thread.frame, 0)
+            self.frameMode = 0
         elif self.frameInverse_btn.isChecked():
-            self.frame_thread.frame = cv2.flip(self.frame_thread.frame, -1)
+            self.frameMode = -1
+        else:
+            self.frameFlip = False
 
     def frame_contrast_brightness_check(self):
         """
@@ -278,8 +306,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         self.frame_thread.frame = cv2.convertScaleAbs(self.frame_thread.Recognition._input_img,alpha=self.contrast,beta=self.brightness)
         self.frame_thread.Recognition.output_img = self.frame_thread.frame
 
-        # frame checking
-        self.frame_check()
+        # frame checking(flip,lightness)
+        if self.frameFlip :
+            self.frame_thread.frame = cv2.flip(self.frame_thread.frame, self.frameMode)
+
         self.lightness_check()
 
         # PyQt image format
@@ -305,20 +335,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         else:
             self.FinishFlag = False
 
-# -----------------------------------------Database-----------------------------------------
-
-    def connectToDB(self):
-        """
-        Connect to Mysql database
-        """
-        try:
-            self.db = pymysql.connect(
-                host=DB_IP, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_DATABASE)
-            print('Connect to database success!')
-        except:
-            print('Conncet to database failed!')
-
-
 # -----------------------------------------Menubar-----------------------------------------
 
     def settingAction_triggered(self):
@@ -331,26 +347,27 @@ class MainWindow(QtWidgets.QMainWindow, Ui_SmartLearningSystemGUI):
         """
         open user guide message box
         """
-        guideText = open(file='./project_codes/GUI/guide.txt',mode='r',encoding='utf-8')
+        self.guideText = open(file='./project_codes/GUI/guide.txt',mode='r',encoding='utf-8')
         QMessageBox(icon=QMessageBox.Information,
                     windowIcon=QIcon('./project_codes/GUI/images/guide_icon.png'),
                     windowTitle='使用說明',
-                    text=guideText.read(),
-                    standardButtons=(QMessageBox.Yes)).exec_()
-        guideText.close()
+                    text=self.guideText.read()).exec()
+        self.guideText.close()
 
     def historyAction_triggerred(self):
         """
-        historyAction_triggerred
-        """
-        print('historyAction_triggerred')
+        setting result_box and translate_box to translate history 
+        """        
+        actionText = self.sender().text()
+        self.result_box.setText(actionText)
+        self.translate_box.setText(self.historyDict[actionText])
     
     def openWordFileAction_triggered(self):
         """
-        openWordFileAction_triggered
+        Open local word dictionary,if file not exist,create one
         """
-        print('openWordFileAction_triggered')
-
+        self.fileOpen_thread = localFileOpen_Thread()
+        self.fileOpen_thread.start()
 
 # -----------------------------------------Threading-----------------------------------------
 
@@ -370,7 +387,6 @@ class frame_Thread(QThread):
             self.frame_callback.emit(1)
             time.sleep(0.01)
             
-
 class gTTS_Thread(QThread):
     """
     google Text To Speech threading
@@ -391,10 +407,49 @@ class translation_Thread(QThread):
         super().__init__(parent)
         self.input_text = input_text
         self.translated_lang = translated_lang
+        self.sentenceOrWord ,self.translation_output = -1,""
 
     def run(self):
         self.sentenceOrWord = selector_TranslateOrWord.check_if_one_word(self.input_text)
         self.translation_output = selector_TranslateOrWord.selector_TranslateOrWord(self.input_text,src='en',dest=self.translated_lang)
         self.translation_finished.emit(1)
 
+
+class connectDB_Thread(QThread):
+    """
+    connect to database threading
+    """
+    def __init__(self,parent=None):
+        super().__init__(parent)
+        self.currentPath = os.path.dirname(__file__)
+        self.dirPath = os.path.split(self.currentPath)[0]    
+        self.filePath = os.path.join(self.dirPath,'localDict.txt')
+
+    def run(self):
+        try:
+            self.db = pymysql.connect(
+                host=DB_IP, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_DATABASE)
+            print('Connect to database success!')
+        except:
+            print('Conncet to database failed!')
+
     
+class localFileOpen_Thread(QThread):
+    """
+    local word file open threading
+    """
+    def __init__(self,parent=None):
+        super().__init__(parent)
+        self.currentPath = os.path.dirname(__file__)
+        self.dirPath = os.path.split(self.currentPath)[0]    
+        self.filePath = os.path.join(self.dirPath,'localDict.txt')
+
+    def run(self):
+        if not os.path.exists(self.filePath):
+            open(self.filePath,'w')
+               
+        try :
+            os.system(self.filePath)
+        except:
+            print('Open file error!')
+
